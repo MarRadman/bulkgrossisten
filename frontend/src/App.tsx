74cf54186e81af613ef25b0a1bca811c34fe3741
MarRadman <martinio@hotmail.se>
2024-05-10 13:34:00 +0200

    Fixed the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,34 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 type User = {
-  id: number;
-  name: string;
+  user_id: number;
   username: string;
+  password_hash: string;
   email: string;
+  phone_number: string;
+  address: string;
 };
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
+  const [userList, setUserList] = useState<User[]>([]);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
-
   useEffect(() => {
-    fetch("/users")
+    fetch("http://localhost:3000/users")
       .then((response) => response.json())
       .then((result) => {
-        setUsers(result);
+        if(result.length === 0 || result === null) {
+          console.log("No users found");
+        }
+        else {
+          setUserList(result);
+          console.log(result);
+        }
       });
   }, []);
 
   const handleUserSelect = (id: number) => {
-    const user = users.find((user) => user.id === id);
+    const user = userList.find((currentUser) => currentUser.user_id === id);
     setSelectedUser(user || null);
   };
 
@@ -30,18 +37,16 @@ function App() {
     <>
       <h1>Users</h1>
       <ul>
-        {users.map((user) => (
-          <li key={user.id} onClick={() => handleUserSelect(user.id)}>
-            {user.name}
+        {userList.map((currentUsers) => (
+          <li key={currentUsers.user_id} onClick={() => handleUserSelect(currentUsers.user_id)}>
+            {currentUsers.username}
+            {currentUsers.email}
+            {currentUsers.phone_number}
+            {currentUsers.password_hash}
+            {currentUsers.address}
           </li>
         ))}
       </ul>
-      {selectedUser && (
-        <div>
-          <h2>{selectedUser.username}</h2>
-          <p>{selectedUser.email}</p>
-        </div>
-      )}
     </>
   );
 }
