a8f9aa8823a5aecd9b4005bdc23bbe8dd3283738
MarRadman <martinio@hotmail.se>
2024-05-12 18:02:07 +0200

    added footer and social media icons

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx'
 import NavMenu from './components/NavMenu.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NavMenu />
     <div id="App">
       <App />
+      <Footer />
     </div>
   </React.StrictMode>,
 )
