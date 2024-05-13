1c804cc31b65e6e2104f191d457f0d5f01053a54
MarRadman <martinio@hotmail.se>
2024-05-13 15:39:48 +0200

    fixed bug in AppView.tsx

--- frontend/src/views/AppView.tsx
+++ frontend/src/views/AppView.tsx
@@ -3,35 +3,7 @@ import  { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import '../assets/App.css';
 
-// type User = {
-//   user_id: number;
-//   username: string;
-//   password_hash: string;
-//   email: string;
-//   phone_number: string;
-//   address: string;
-// };
-
 function App() {
-  // const [userList, setUserList] = useState<User[]>([]);
-  // const [selectedUser, setSelectedUser] = useState<User | null>(null);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await fetch("http://localhost:3000/users");
-  //     const result = await response.json();
-  //     if(result.length === 0 || result === null) {
-  //       console.log("No users found");
-  //     }
-  //     else {
-  //       setUserList(result);
-  //       console.log(result);
-  //     }
-  //   };
-
-  //   fetchData();
-  // }, []);
-
   return (
     <React.Fragment>
       <div className="appView_photos">
