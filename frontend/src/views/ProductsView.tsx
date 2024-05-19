9ddca26ca74375fdfb483b09cebb1d4cf4cba719
MarRadman <martinio@hotmail.se>
2024-05-19 10:09:42 +0200

    Fixed ProductsView

--- frontend/src/views/ProductsView.tsx
+++ frontend/src/views/ProductsView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { ListGroup, Button } from 'react-bootstrap';
+import { ListGroup, Button , Modal} from 'react-bootstrap';
 import withAuthCheck from '../authentication/withAuthCheck';
 import BackBtn from '../components/BackBtn';
 
@@ -13,7 +13,9 @@ type Product = {
 
 function ProductsView() {
   const [productList, setProductList] = useState<Product[]>([]);
-  const [selectedCartItem, setSelectedCartItem] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [cartItems, setCartItems] = useState<Product[]>([]);
 
   useEffect(() => {
     const fetchProductData = async () => {
@@ -35,34 +37,65 @@ function ProductsView() {
       }
       else {
         setProductList(result);
-        console.log(result);
+        // console.log(result);
       }
     };
 
     fetchProductData();
   },[]);
 
-  function handleClick() {
-    console.log("Item added to cart", productList);
-    setSelectedCartItem(productList); //TODO add selected item to cart
+  function handleClick(product: Product) {
+    if (!product) {
+      console.log('Product is undefined');
+      return;
+    }
+    console.log('Product:', product);
+    if (!product.name) {
+      console.log('Product name is undefined');
+      return;
+    }
+    console.log("Item added to cart", product.name);
+    setCartItems(prevItems => [...prevItems, product]);
   }
+
+  const handleShowModal = (product: Product) => {
+    setSelectedProduct(product);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <React.Fragment>
-      <h2>Products</h2>
-         <ListGroup>
-          {productList.length > 0 && productList.map((product) => (
-            <ListGroup.Item action key={product.product_id}>
-              <h2>{product.name}</h2>
-              <p>Beskriving: {product.description}</p>
-              <p>Pris: {product.price}kr</p>
-              <Button onClick={handleClick} >Add to Cart</Button> {/*TODO add onClick event to add item to cart*/}
-            </ListGroup.Item>
-          ))}
-        </ListGroup>
-        <BackBtn />
-      </React.Fragment>
-    );
+    <h2>Products</h2>
+    <ListGroup>
+      {productList.length > 0 && productList.map((product) => (
+        <ListGroup.Item action key={product.product_id}>
+          <h2>{product.name}</h2>
+          <p>Pris: {product.price}kr</p>
+          <div>
+          <Button onClick={() => handleClick(product)}>Add to Cart</Button>
+          <Button variant="info" onClick={() => handleShowModal(product)}>Info</Button>
+          </div>
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+      <Modal show={showModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+        <Modal.Title>{selectedProduct?.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{selectedProduct?.description}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <BackBtn />
+    </React.Fragment>
+  );
 }
 
 export default withAuthCheck(ProductsView);
