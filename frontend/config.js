aa3978c1a6388819124351318f48c3030ffecf89
MarRadman <martinio@hotmail.se>
2024-05-23 19:23:50 +0200

    fixed config

--- frontend/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:3000'
-};
-exports.default = config;
