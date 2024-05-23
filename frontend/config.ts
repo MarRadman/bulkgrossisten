2fc8075f767be71bfa01334eb620e9059cb70bd7
MarRadman <martinio@hotmail.se>
2024-05-23 22:55:32 +0200

    fixed

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,5 +1,13 @@
+let apiUrl;
+
+if (process.env.NODE_ENV === "development") {
+  apiUrl = "http://localhost:3000";
+} else if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://bulkgrossisten.onrender.com";
+}
+
 const config = {
-  apiUrl: process.env.REACT_APP_API_URL || 'http://localhost:3000'
+  apiUrl
 };
 
 export default config;
